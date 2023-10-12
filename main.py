b23a98c104a61c65df8cd6505fea9e9421546574
TonyGrif <TonyGriffin2000@gmail.com>
2023-10-11 21:54:39 -0400

    require command line argument

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
+import argparse
+from pathlib import Path
+
 def main():
-    print("Hello World!")
+    parser = argparse.ArgumentParser(
+            prog="cpu-temperature",
+            description="Python program to analyze CPU temperature changes over time")
+
+    parser.add_argument(
+            'txt_file',
+            type=Path,
+            help="Path to text file")
+
+    args = parser.parse_args()
+
+    print(args.txt_file)
     return
 
 if __name__ == "__main__":
