63f6e9bb1bd1fd7586efb4b30910a34c2567566b
TonyGrif <TonyGriffin2000@gmail.com>
2023-10-23 17:06:26 -0400

    run linters on module

--- src/piecewise.py
+++ src/piecewise.py
@@ -1,3 +1,8 @@
+"""
+This module contains the piecewise linear interpolation functionality.
+"""
+
+
 def piecewise_linear_interpolation(
     x_zero: int, x_one: int, y_zero: int, y_one: int
 ) -> str:
--- tests/test_piecewise.py
+++ tests/test_piecewise.py
@@ -3,6 +3,7 @@ import pytest
 
 from piecewise import piecewise_linear_interpolation
 
+
 class TestPiecewiseLinearInterpolation:
     def test_equation(self):
         result = piecewise_linear_interpolation(30, 60, 80, 62)
@@ -10,6 +11,6 @@ class TestPiecewiseLinearInterpolation:
 
         result = piecewise_linear_interpolation(30, 60, 77, 60)
         assert result == "94.0000 + -0.5667x"
-    
+
         result = piecewise_linear_interpolation(0, 30, 61, 80)
         assert result == "61.0000 + 0.6333x"
