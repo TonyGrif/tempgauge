93c2c23d6c39694e66c4b1bb450aba6362dfb301
TonyGrif <TonyGriffin2000@gmail.com>
2023-10-24 18:15:27 -0400

    refactor formatted string output

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
 import argparse
 from pathlib import Path
 
-
 from parse_temps import parse_raw_temps
 from src.piecewise import piecewise_linear_interpolation
 
@@ -26,7 +25,6 @@ def main():
         print("Must supply a real text file")
         return
 
-    # TODO: restructure
     temp_data = []
 
     with open(args.txt_file, "r", encoding="utf-8") as temps:
@@ -34,23 +32,28 @@ def main():
             # Catch new lines at the end
             if not f_temps[1]:
                 break
+            # Stores as (time, [temps...])
             temp_data.append(f_temps)
 
     for core_num in range(4):
         print(f"Core {core_num} \n============")
-        for count, time in enumerate(temp_data):
+        for count, _ in enumerate(temp_data):
             if count + 1 > len(temp_data) - 1:
                 break
 
             start_time = temp_data[count][0]
-            end_time = temp_data[count+1][0]
+            end_time = temp_data[count + 1][0]
 
             start_temp = temp_data[count][1][core_num]
-            end_temp = temp_data[count+1][1][core_num]
+            end_temp = temp_data[count + 1][1][core_num]
+
+            pli_eq = piecewise_linear_interpolation(
+                start_time, end_time, start_temp, end_temp
+            )
 
             print(
-                f"{start_time} <= x <= {end_time}; "
-                + f"y= {piecewise_linear_interpolation(start_time, end_time, start_temp, end_temp)}; "
+                f"{start_time: <6} <= x <= {end_time: >6}; "
+                + f"y= {pli_eq: <20}; "
                 + "interpolation"
             )
 
