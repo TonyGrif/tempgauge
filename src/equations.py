3ed00012c9c2d5e0a54bc8c799fc2ef73e4074dc
TonyGrif <TonyGriffin2000@gmail.com>
2023-11-13 17:50:36 -0500

    add numpy to requirements

--- src/equations.py
+++ src/equations.py
@@ -1,13 +1,14 @@
+"""This module contains the piecewise linear interpolation and
+least squares approximation functionality.
 """
-This module contains the piecewise linear interpolation functionality.
-"""
+
+import numpy as np
 
 
 def piecewise_linear_interpolation(
     x_zero: int, x_one: int, y_zero: int, y_one: int
 ) -> str:
-    """
-    Calculate the piecewise linear interpolation for two points.
+    """Calculate the piecewise linear interpolation for two points.
 
     Paramaters:
         x_zero (int): Starting time point.
@@ -26,3 +27,8 @@ def piecewise_linear_interpolation(
     equation = f"{y_intercept} + {format(slope, '.4f')}x"
 
     return equation
+
+
+def least_squares_transpose() -> None:
+    """Calculate the discrete least squares approximation for given points."""
+    return None
--- tests/test_equations.py
+++ tests/test_equations.py
@@ -1,11 +1,16 @@
 import pytest
 
 from equations import piecewise_linear_interpolation
+from equations import least_squares_transpose
 
+@pytest.fixture
+def points():
+    points = [(30, 80), (60, 62)]
+    return points
 
 class TestEquations:
-    def test_piecewise_linear_interpolation(self):
-        result = piecewise_linear_interpolation(30, 60, 80, 62)
+    def test_piecewise_linear_interpolation(self, points):
+        result = piecewise_linear_interpolation(points[0][0], points[1][0], points[0][1], points[1][1])
         assert result == "98.0000 + -0.6000x"
 
         result = piecewise_linear_interpolation(30, 60, 77, 60)
@@ -13,3 +18,6 @@ class TestEquations:
 
         result = piecewise_linear_interpolation(0, 30, 61, 80)
         assert result == "61.0000 + 0.6333x"
+
+    def test_least_squares_transpose(self, points):
+        assert least_squares_transpose() is None
