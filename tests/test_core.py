1e57fec2d147f33c743963d8f57e0bfa55ed7893
TonyGrif <TonyGriffin2000@gmail.com>
2026-05-18 21:58:57 -0400

    build(poetry): use just ruff as formatter and linter with mypy

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,3 @@
-import os.path
-
 import pytest
 
 from tempgauge.core import Core
@@ -32,7 +30,6 @@ class TestCore:
 
         with pytest.raises(ValueError):
             core.core_num = -3
-            assert not core.readings
 
     def test_add_reading(self, core):
         assert core.core_num == 1
@@ -47,33 +44,27 @@ class TestCore:
 
     def test_str(self, full_core):
         string = str(full_core)
-        assert "0" in string and "30" in string
+        assert "0      <= x <=     30" in string
         assert "61.0000" in string and "0.6333" in string
         assert "interpolation" in string
 
-        assert "0" in string and "120" in string
+        assert "0      <= x <=    120" in string
         assert "67.4000" in string and "0.0567" in string
         assert "least-squares" in string
 
-    def test_write_to_file(self, core):
-        assert core.core_num == 1
-        assert not core.readings
+    def test_str_empty(self, core):
+        assert str(core) == ""
 
-        core.add_reading((0, 61))
-        core.add_reading((30, 80))
-        core.add_reading((60, 62))
-        core.add_reading((90, 83))
-        core.add_reading((120, 68))
+    def test_write_to_file(self, full_core, tmp_path):
+        full_core.write_to_file(tmp_path)
+        out = tmp_path / "core-0.txt"
+        assert out.is_file()
 
-        core.write_to_file()
-        assert os.path.isfile("reports/core-1.txt")
+        lines = out.read_text(encoding="UTF-8").splitlines()
+        assert "0      <= x <=     30" in lines[0]
+        assert "61.0000" in lines[0] and "0.6333" in lines[0]
+        assert "interpolation" in lines[0]
 
-        with open("reports/core-1.txt", "r") as file:
-            lines = file.readlines()
-            assert "0" in lines[0] and "30" in lines[0]
-            assert "61.0000" in lines[0] and "0.6333" in lines[0]
-            assert "interpolation" in lines[0]
-
-            assert "0" in lines[-1] and "120" in lines[-1]
-            assert "67.4000" in lines[-1] and "0.0567" in lines[-1]
-            assert "least-squares" in lines[-1]
+        assert "0      <= x <=    120" in lines[-1]
+        assert "67.4000" in lines[-1] and "0.0567" in lines[-1]
+        assert "least-squares" in lines[-1]
